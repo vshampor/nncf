2aa7fb107f83df92db80cde99e6f00dc1a87dfb2
Vasily Shamporov <vasily.shamporov@intel.com>
2024-01-02 17:23:30 +0100

    Remove FDP

--- tests/fuzz/fuzz_nncfconfig_parsing.py
+++ tests/fuzz/fuzz_nncfconfig_parsing.py
@@ -12,8 +12,11 @@ set_log_level(logging.CRITICAL)
 
 @atheris.instrument_func
 def TestNNCFConfigParsing(input_bytes):
-    fdp = atheris.FuzzedDataProvider(input_bytes)
-    unicode_str = fdp.ConsumeUnicode(sys.maxsize)
+    try:
+        unicode_str = input_bytes.decode('utf8')
+    except:
+        return
+
     try:
         parsed_dict = jstyleson.loads(unicode_str)
     except json.JSONDecodeError:
@@ -23,6 +26,17 @@ def TestNNCFConfigParsing(input_bytes):
         NNCFConfig.from_dict(parsed_dict)
     except jsonschema.ValidationError:
         return
+
+# def json_mutator(data, max_size, seed):
+#     fdp = atheris.FuzzedDataProvider(data)
+#     unicode_str = fdp.ConsumeUnicode(sys.maxsize)
+#     try:
+#         decompressed = jstyleson.loads(unicode_str)
+#     except json.JSONDecodeError:
+#         parsed_dict = {}
+#     else:
+#         decompressed = atheris.Mutate(decompressed, len(decompressed))
+#     return zlib.compress(decompressed)
 
 
 atheris.Setup(sys.argv, TestNNCFConfigParsing)
